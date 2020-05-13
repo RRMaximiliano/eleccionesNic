15bfdaa47e09d47a92d8e99005c282d0701e4537
Rony <maximiliano.rod.ra@gmail.com>
2020-05-13 02:22:38 -0400

    Fix some errores related to yml files

--- R/data.R
+++ R/data.R
@@ -1,2 +1,7 @@
-#' Elecciones presidencial en Nicaragua
+#' @title Elecciones presidenciales en Nicaragua
+#' @description Tibble de las elecciones desde 1990 al 2006
+#' @format A data frame with 72 observartions and 35 variables:
+#' @author Rony Rodriguez-Ramirez
+#' @source
+#' @references
 "nic_elecciones"
